befb0bad3414d8976195207fd3f6d0664e76dc89
OscarRibeiro07 <negociante2t@gmail.com>
2025-06-24 09:10:38 -0300

    implementa classe comparator na classe de pessoa

--- src/br/com/dio/list/ordenacao/Pessoa.java
+++ src/br/com/dio/list/ordenacao/Pessoa.java
@@ -1,15 +1,21 @@
 package br.com.dio.list.ordenacao;
 
-public class Pessoa {
+public class Pessoa implements  Comparable<Pessoa>{
 
     private String nome;
     private int idade;
     private double altura;
 
+
     public Pessoa(String nome, int idade, double altura) {
         this.nome = nome;
         this.idade = idade;
         this.altura = altura;
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        return 0;
     }
 
     @Override
