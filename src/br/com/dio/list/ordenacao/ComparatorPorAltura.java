68ef2271944eebf7ee137965408c43d30f8a3f7d
OscarRibeiro07 <negociante2t@gmail.com>
2025-06-24 09:24:06 -0300

    implementa metodo compare da classe comparator

--- src/br/com/dio/list/ordenacao/ComparatorPorAltura.java
+++ src/br/com/dio/list/ordenacao/ComparatorPorAltura.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class ComparatorPorAltura implements Comparator<Pessoa> {
     @Override
     public int compare(Pessoa o1, Pessoa o2) {
-        return 0;
+        return Double.compare(o1.getAltura(), o2.getAltura());
     }
 }
