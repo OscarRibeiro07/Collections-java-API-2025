dc2b101df6f3de0eb7eaf3d384381c53377112bb
OscarRibeiro07 <negociante2t@gmail.com>
2025-06-24 09:35:27 -0300

    ajusta algumas sintaxe no contrutor de ordenacao e cria main para realizar testes de cadastros e metodos criados

--- src/br/com/dio/list/ordenacao/OrdenacaoPessoa.java
+++ src/br/com/dio/list/ordenacao/OrdenacaoPessoa.java
@@ -1,14 +1,13 @@
 package br.com.dio.list.ordenacao;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class OrdenacaoPessoa {
     private List<Pessoa> pessoaList;
 
-    public OrdenacaoPessoa(List<Pessoa> pessoaList) {
+    public OrdenacaoPessoa() {
         this.pessoaList = new ArrayList<>();
     }
 
@@ -16,12 +15,29 @@ public class OrdenacaoPessoa {
         pessoaList.add(new Pessoa(nome, idade, altura));
     }
 
-    public List<Pessoa> ordenaPorIdade(int idade){
+    public List<Pessoa> ordenaPorIdade(){
         List<Pessoa> pessoaPorIdade=new ArrayList<>(pessoaList);
         Collections.sort(pessoaPorIdade);
         return pessoaPorIdade;
     }
 
+    public  List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoaPorAltura=new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
 
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        System.out.println("***********CADASTRO DE PESSOAS***********");
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 15,1.86);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30,1.87);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 20,1.88);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 25,2.00);
+        System.out.println("***********ORDENAÇÃO POR IDADE***********");
+        System.out.println(ordenacaoPessoa.ordenaPorIdade());
+        System.out.println("***********ORDENAÇÃO POR ALTURA***********");
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
 
 }
