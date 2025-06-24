4bb9fc5b733bba3c9d0de1734d1591217ab217ea
OscarRibeiro07 <negociante2t@gmail.com>
2025-06-24 09:08:02 -0300

    implementa classe de ordenacao pessoa e cria metodo de  adicionar pessoa

--- src/br/com/dio/list/ordenacao/OrdenacaoPessoa.java
+++ src/br/com/dio/list/ordenacao/OrdenacaoPessoa.java
@@ -1,4 +1,18 @@
 package br.com.dio.list.ordenacao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrdenacaoPessoa {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa(List<Pessoa> pessoaList) {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+
 }
