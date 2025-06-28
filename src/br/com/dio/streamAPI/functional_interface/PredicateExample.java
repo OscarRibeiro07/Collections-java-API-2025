867f3b096f4e2a0a81e460619fed81ed38327173
OscarRibeiro07 <negociante2t@gmail.com>
2025-06-28 04:59:17 -0300

    realiza minha propria implementacao no pradicate como teste de uso

--- src/br/com/dio/streamAPI/functional_interface/PredicateExample.java
+++ src/br/com/dio/streamAPI/functional_interface/PredicateExample.java
@@ -2,6 +2,7 @@ package br.com.dio.streamAPI.functional_interface;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Predicate;
 
 /**
@@ -10,6 +11,7 @@ import java.util.function.Predicate;
  */
 public class PredicateExample {
   public static void main(String[] args) {
+    System.out.println("*****EXEMPLO DA AULA*****");
     // Criar uma lista de palavras
     List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
 
@@ -19,7 +21,34 @@ public class PredicateExample {
     // Usar o Stream para filtrar as palavras com mais de 5 caracteres e, em seguida,
     // imprimir cada palavra que passou no filtro
     palavras.stream()
-        .filter(maisDeCincoCaracteres)
-        .forEach(System.out::println);
+            .filter(maisDeCincoCaracteres)
+            .forEach(System.out::println);
+
+
+
+
+    List<String> comparaPalavra = Arrays.asList("Jabuticaba", "Jacaranda", "Jujuba", "Gelo", "Agua");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Informe o caracter ou o conjunto decaracter  que deseja comparar");
+    String nome = sc.next();
+    System.out.println("*****teste de pradicate*****");
+    Predicate<String> quantidadeDeCaracters = letra ->letra.length()>4;
+    Predicate<String> listaPorLetra = letra ->letra.contains(nome);
+
+    System.out.println("Stream por quantidade de letras");
+    comparaPalavra.stream()
+            .filter(quantidadeDeCaracters)
+            .forEach(System.out::println);
+
+
+    System.out.println("Stream por letra especifica");
+    comparaPalavra.stream()
+            .filter(listaPorLetra)
+            .forEach(System.out::println);
+
+
+
+
   }
+
 }
